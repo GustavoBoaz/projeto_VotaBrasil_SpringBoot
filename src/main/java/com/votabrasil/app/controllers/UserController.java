f36317f907a488890d15c2871491d620c4c156af
raizapaula93 <raizapaula93@gmail.com>
2022-02-21 10:53:46 -0300

    @RaizaPaula93 [Task: CREATE Post Put Delete UserController

--- src/main/java/com/votabrasil/app/controllers/UserController.java
+++ src/main/java/com/votabrasil/app/controllers/UserController.java
@@ -3,8 +3,12 @@ package com.votabrasil.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,20 @@ public class UserController {
 	public UserModel getById(@PathVariable(value = "id") Long idUser) {
 		return repository.findById(idUser).get();
 	}
-
+	
+	@PostMapping
+	public UserModel save(@RequestBody UserModel user) {
+		return repository.save(user);
+	}
+	
+	@PutMapping
+	public UserModel update(@RequestBody UserModel user) {
+		return repository.save(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable(value = "id") Long idUser) {
+		repository.deleteById(idUser);
+	}
+	
 }
